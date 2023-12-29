94a1f721ff6525321da689de6736e23c75295304
Gaudiosi <g.gaudiosi1@studenti.unisa.it>
2023-12-29 22:04:59 +0100

    Create Navbar

---
+++ src/Segnalazione.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Segnalazione = () => {
+  return (
+    <div>
+      <h2>Segnalazione</h2>
+      <p>Contenuto della pagina Segnalazione...</p>
+    </div>
+  );
+}
+
+export default Segnalazione;

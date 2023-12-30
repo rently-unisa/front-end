65544d5ee0215a6d04dc63a4cac8edd90ccfad1d
Gaudiosi <g.gaudiosi1@studenti.unisa.it>
2023-12-31 00:39:53 +0100

    create ChiSiamo page

--- src/ChiSiamo.js
+++ src/ChiSiamo.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React from "react";
+import "./style/ChiSiamo.css";
+import onda1 from "./image/onda1.svg";
+import onda2 from "./image/onda2.svg";
+import persone from "./image/persone1.svg";
+import citta from "./image/citta1.svg";
+import frecce from "./image/frecce1.svg";
 
 const ChiSiamo = () => {
   return (
-    <div>
-      <h2>Chi Siamo</h2>
-      <p>Contenuto della pagina Chi Siamo...</p>
+    <div className="Page">
+      <div className="top-right">
+        <img src={onda2} alt="Immagine decortiva" />
+      </div>
+
+      <div className="container">
+        <h1 className="titolo">Chi Siamo</h1>
+        <div className="divisione1">
+          <div className="container-pinfo">
+            <div className="pinfo">
+              L'attenzione all'
+              <span style={{ color: "var(--verde)" }}>ambiente</span> e
+              l'opportunità di{" "}
+              <span style={{ color: "var(--verde)" }}>risparmio</span> (e
+              guadagno) sono i principi su cui si fonda Rently.
+            </div>
+          </div>
+          <img src={persone} alt="Immagine decortiva" />
+        </div>
+        <div className="divisione2">
+          <img src={frecce} alt="Immagine decortiva" />
+          <div className="container-pinfo2">
+            <div className="pinfo2">
+              Su Rently:
+              <ul style={{ marginTop: "0" }}>
+                <li>
+                  Metti a noleggio oggetti che non usi e guadagni con essi.
+                </li>
+                <li>
+                  Noleggi ciò di cui hai bisogno, risparmiando notevolmente.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="divisione3">
+          <div className="container-pinfo3">
+            <div className="pinfo3">
+              In un mondo sensibile all'ecologia, questa è un'opportunità
+              concreta per valorizzare gli oggetti poco usati, evitare acquisti
+              inutili e ridurre le emissioni nocive dovute alla produzione di
+              nuovi oggetti.
+            </div>
+          </div>
+          <img src={citta} alt="Immagine decortiva" />
+        </div>
+      </div>
+
+      <div className="bottom-left">
+        <img src={onda1} alt="Immagine decortiva" />
+      </div>
     </div>
   );
-}
+};
 
 export default ChiSiamo;
